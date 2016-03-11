96ac46eb7046ac33ab0a0d75a8417db46edd4868
Alexandr.Pirs <alexandr.pirss@mail.ru>
2016-03-11 17:46:18 +0400

    site uber #1

---
+++ uber_1/send.php
@@ -0,0 +1,15 @@
+<?
+$phone = $_POST['tell'];
+$name = $_POST['name'];
+$message = "Телефон: {$phone} Имя: {$name}";
+// На случай если какая-то строка письма длиннее 70 символов мы используем wordwrap()
+$message = wordwrap($message, 70, "\r\n");
+// Отправляем
+$success = mail('uber@rabotauber.ru', 'Заполнена заявка с сайта Уберпартнер.рф', $message);
+
+if($success) {
+    echo 1;
+} else {
+    echo 0;
+}
+?>
---
+++ uber_1/send_promocode.php
@@ -0,0 +1,15 @@
+<?
+$phone = $_POST['userName'];
+$name = $_POST['userPhone'];
+$promo = $_POST['promoCode'];
+$message = "Телефон: {$phone} Имя: {$name} Промокод: {$promo}";
+// На случай если какая-то строка письма длиннее 70 символов мы используем wordwrap()
+$message = wordwrap($message, 70, "\r\n");
+// Отправляем
+$success = mail('alexandr.js@mail.ru', 'Заполнена заявка с промокодом с сайта Уберпартнер.рф', $message);
+
+if($success) {
+    echo 1;
+} else {
+    echo 0;
+}
