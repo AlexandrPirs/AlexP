2e457c8b4780186dd176e6bf0e1c0c9980b93ed1
AlexandrPirs <alexandr.pirss@mail.ru>
2016-04-06 03:50:38 +0400

    1

---
+++ djifor/js/common.js
@@ -0,0 +1,60 @@
+$(function() {
+
+	$(".owl-carousel").owlCarousel({
+		items : 1,
+		nav : true,
+		smartSpeed : 1000,
+		navText : ""
+	});
+
+
+	//search
+	$("header .header_icons .search").click(function()	{
+		$("header .header_icons .input_search input").slideToggle();
+	});
+
+	//SVG Fallback
+	if(!Modernizr.svg) {
+		$("img[src*='svg']").attr("src", function() {
+			return $(this).attr("src").replace(".svg", ".png");
+		});
+	};
+
+	//E-mail Ajax Send
+	//Documentation & Example: https://github.com/agragregra/uniMail
+	$("form").submit(function() { //Change
+		var th = $(this);
+		$.ajax({
+			type: "POST",
+			url: "mail.php", //Change
+			data: th.serialize()
+		}).done(function() {
+			alert("Thank you!");
+			setTimeout(function() {
+				// Done Functions
+				th.trigger("reset");
+			}, 1000);
+		});
+		return false;
+	});
+
+	//Chrome Smooth Scroll
+	try {
+		$.browserSelector();
+		if($("html").hasClass("chrome")) {
+			$.smoothScroll();
+		}
+	} catch(err) {
+
+	};
+
+	$("img, a").on("dragstart", function(event) { event.preventDefault(); });
+	
+});
+
+$(window).load(function() {
+
+	$(".loader_inner").fadeOut();
+	$(".loader").delay(400).fadeOut("slow");
+
+});
